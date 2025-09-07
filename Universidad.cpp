f160a9e49579995fe28efcdf7f841931f4b646a4
Fifthtaschenmesser4 <frome004@gmail.com>
2025-09-07 16:24:20 -0300

    Update Universidad.cpp
    se definen los métodos insertarCurso y recorrerCurso

--- Universidad.cpp
+++ Universidad.cpp
@@ -5,3 +5,24 @@ Universidad::Universidad(){
     headAlumnos = nullptr;
     headCursos = nullptr;
 }
+void Universidad::insertarCurso(Curso curso){
+    NodoCurso* nuevoNodo = new NodoCurso(curso);
+    if(headCursos==nullptr){
+        headCursos=nuevoNodo;
+        return;
+    }
+    NodoCurso* aux = headCursos;
+    while(aux->sigCurso!=nullptr){
+        aux=aux->sigCurso;
+    }
+    aux->sigCurso=nuevoNodo;
+    return;
+}
+
+void Universidad::recorrerCursos(){
+    NodoCurso* aux = headCursos;
+    while(aux!=nullptr){
+        cout<<aux->getCurso().toString()<<endl;
+        aux = aux->sigCurso;
+    }
+}
