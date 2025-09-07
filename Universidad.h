7181973759e1ab8d28bad15b031d3f701d46bb37
Fifthtaschenmesser4 <frome004@gmail.com>
2025-09-06 23:57:06 -0400

    Creación header Universidad
    se tiene un puntero head NodoAlumno de una lista enlazada con todos los alumnos, un puntero head NodoCurso de una lista enlazada con todos los cursos. Tiene los métodos de buscar curso y alumno por id y nombre, eliminar curso y alumno por id, e insertar curso y alumno a las listas.

---
+++ Universidad.h
@@ -0,0 +1,22 @@
+#include "Curso.h"
+#include "NodoCurso.h"
+#include <string>
+
+//Clase que funciona como lista enlazada de cursos, con un puntero head.
+class Universidad{
+    public:
+    Universidad();
+    NodoCurso* headCursos;
+    NodoAlumno* headAlumnos;
+
+    void insertarCurso(NodoCurso);
+    void insertarAlumno(NodoAlumno);
+
+    Curso buscarCursoID(string);
+    Curso buscarCursoNombre(string);
+    void eliminarCursoID(string);
+
+    Alumno buscarAlumnoNombre(string);
+    Alumno buscarAlumnoID(string);
+    void eliminarAlumnoID(string);
+};
