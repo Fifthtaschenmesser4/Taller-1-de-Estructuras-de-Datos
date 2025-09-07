4e67b1378447fe86aa76c7ebedcc01574a38b204
Fifthtaschenmesser4 <frome004@gmail.com>
2025-09-06 23:47:54 -0400

    Se agrega cpp de Alumno
    Se define constructor, métodos getter y toString.

---
+++ Alumno.cpp
@@ -0,0 +1,28 @@
+#include "Alumno.h"
+#include <iostream>
+
+Alumno::Alumno(string nombre, string apellido, string carrera, string fechaIngreso){
+    this -> nombre = nombre;
+    this -> apellido = apellido;
+    this -> carrera= carrera;
+    this -> fechaIngreso = fechaIngreso;
+}
+
+string Alumno::getID(){
+    return id;
+}
+string Alumno::getNombre(){
+    return nombre;
+}
+string Alumno::getApellido(){
+    return apellido;
+}
+string Alumno::getCarrera(){
+    return carrera;
+}
+string Alumno::getFecha(){
+    return fechaIngreso;
+}
+void Alumno::toString(){
+    std::cout<<nombre<<" "<<apellido<<", "<<carrera<<", ingreso: "<<fechaIngreso<<endl;
+}
