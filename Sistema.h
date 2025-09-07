125915cf9fa203e06c09250e45213b4836111e51
Fifthtaschenmesser4 <frome004@gmail.com>
2025-09-07 21:41:17 +0000

    Se crear header Sistema

---
+++ Sistema.h
@@ -0,0 +1,15 @@
+#include "Alumno.h"
+#include "Curso.h"
+#include "NodoAlumno.h"
+#include "NodoCurso.h"
+#include "Universidad.h"
+
+class Sistema{
+    public:
+    Universidad getInstance();
+    crearAlumno(string,string,string,string);
+    crearCurso(string,int,string,string);
+    private:
+    Universidad();
+    Universidad universidad;
+};
