c9fcfa2885b1ff9b11cf5b61267b1c2360b690db
Fifthtaschenmesser4 <frome004@gmail.com>
2025-09-06 23:32:27 -0400

    Creación Header Alumno
    Se crea el header de la clase Alumno, con sus atributos id, nombre, apellido, carrera, fechaIngreso, y método toString().

---
+++ Alumno.h
@@ -0,0 +1,16 @@
+#pragma one
+#include <string>
+using namespace std;
+//identificador único, nombre, apellido, carrera y fecha de ingreso
+class Alumno{
+    private:
+    string id, nombre, apellido, carrera, fechaIngreso;
+    public:
+    Alumno(string,string,string,string);
+    string getID();
+    string getNombre();
+    string getApellido();
+    string getCarrera();
+    string getFecha();
+    void toString();
+};
